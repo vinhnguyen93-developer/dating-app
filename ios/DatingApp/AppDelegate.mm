3a64e0037629f2380c577f25f541f55d1668667a
Vinhnguyen93-developer <developerthienkim@gmail.com>
2022-09-23 10:04:32 +0700

    [Implement] Setup firebase for IOS

--- ios/DatingApp/AppDelegate.mm
+++ ios/DatingApp/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -32,6 +33,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTAppSetupPrepareApp(application);
+  [FIRApp configure];
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
 
